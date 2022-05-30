5369647bfede7703635d26f301dfb610333fdcc5
souraavsen1 <sourav15-11392@diu.edu.bd>
2022-05-31 03:03:42 +0600

    feat: loader added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Home from "./Components/Pages/Home";
 import NavSection from "./Components/Shared/NavSection";
-
+import Loader from "./asstes/Images/loader.gif";
 function App() {
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 2000);
+  }, []);
+
   return (
     <div>
-      <NavSection />
-      <Home />
+      {loader ? (
+        <div className='w-full h-screen flex justify-center items-center'>
+          <img src={Loader} alt='' />
+        </div>
+      ) : (
+        <>
+          <NavSection />
+          <Home />
+        </>
+      )}
     </div>
   );
 }
